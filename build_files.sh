5dcc1a6f9e7fbc2e9cd50265e484513db58b6faa
Christine <2023bcs004@std.must.ac.ug>
2025-12-13 20:04:11 +0300

    fix(vercel): explicitly create output directory

--- build_files.sh
+++ build_files.sh
@@ -4,6 +4,9 @@
 echo "Building the project..."
 python3 -m pip install -r requirements.txt
 
+# Create staticfiles_build directory explicitly
+mkdir -p staticfiles_build
+
 # Collect static files
 echo "Collect static..."
 python3 manage.py collectstatic --noinput --clear
