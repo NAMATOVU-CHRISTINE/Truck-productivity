faac1a362c55e35eb47a774dedfa95c082a22777
Christine <2023bcs004@std.must.ac.ug>
2025-12-13 19:52:20 +0300

    fix(vercel): remove migrations from build script to fix build error

--- build_files.sh
+++ build_files.sh
@@ -4,11 +4,6 @@
 echo "Building the project..."
 python3 -m pip install -r requirements.txt
 
-# Make migrations
-echo "Make migrations..."
-python3 manage.py makemigrations
-python3 manage.py migrate
-
 # Collect static files
 echo "Collect static..."
 python3 manage.py collectstatic --noinput --clear
